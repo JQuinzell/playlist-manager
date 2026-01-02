3dd526d48398ea0edd7d1b83c084b01752239241
jquinzell <>
2026-01-01 23:58:23 -0600

    select rows

---
+++ src/app/playlists/[id]/PlaylistItemTable.tsx
@@ -0,0 +1,97 @@
+'use client'
+import { EllipsisVertical } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { PlaylistCommand } from '@/PlaylistCommand'
+import type { Playlist, PlaylistItem } from '@/youtube'
+
+type Props = {
+  items: PlaylistItem[]
+  playlists: Playlist[]
+}
+
+export const PlaylistItemTable: React.FC<Props> = ({ items, playlists }) => {
+  const [selectedItems, setSelectedItems] = useState<Record<string, boolean>>(
+    {}
+  )
+
+  function toggleItem(item: PlaylistItem, selected: boolean) {
+    setSelectedItems((prev) => ({
+      ...prev,
+      [item.id]: selected,
+    }))
+  }
+
+  return (
+    <Table>
+      <TableCaption>Videos</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead></TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {items.map((item) => (
+          <TableRow
+            key={item.id}
+            data-state={selectedItems[item.id] ? 'selected' : ''}
+          >
+            <TableCell>
+              <Checkbox
+                onCheckedChange={(state) =>
+                  // handle indeterminate state
+                  toggleItem(item, typeof state === 'boolean' ? state : false)
+                }
+              />
+            </TableCell>
+            <TableCell>
+              {item.thumbnail && (
+                <Image
+                  src={item.thumbnail.url}
+                  width={120}
+                  height={68}
+                  alt={item.title}
+                  className='object-cover'
+                ></Image>
+              )}
+            </TableCell>
+            <TableCell>
+              <Link href={`/playlists/${item.id}`}>{item.title}</Link>
+            </TableCell>
+            <TableCell>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button size='icon'>
+                    <EllipsisVertical />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PlaylistCommand playlists={playlists} item={item} />
+                </PopoverContent>
+              </Popover>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
--- src/app/playlists/[id]/page.tsx
+++ src/app/playlists/[id]/page.tsx
@@ -1,96 +1,18 @@
-import { EllipsisVertical } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { Checkbox } from '@/components/ui/checkbox'
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from '@/components/ui/command'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
-import { PlaylistCommand } from '@/PlaylistCommand'
-import { getItems, insertItem, listPlaylists } from '@/youtube'
+import { getItems, listPlaylists, type PlaylistItem } from '@/youtube'
+import { PlaylistItemTable } from './PlaylistItemTable'
 export default async function PlaylistPage({
   params,
 }: {
   params: Promise<{ id: string }>
 }) {
   const { id } = await params
+  // TODO: should move to a context provider
   const items = await getItems(id)
   const playlists = await listPlaylists()
 
   return (
     <div>
-      <Table>
-        <TableCaption>Videos</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead></TableHead>
-            <TableHead>Title</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {items.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>
-                {item.thumbnail && (
-                  <Image
-                    src={item.thumbnail.url}
-                    width={240}
-                    height={135}
-                    alt={item.title}
-                    className='object-cover'
-                  ></Image>
-                )}
-              </TableCell>
-              <TableCell>
-                <Link href={`/playlists/${item.id}`}>{item.title}</Link>
-              </TableCell>
-              <TableCell>
-                <Checkbox />
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button size='icon'>
-                      <EllipsisVertical />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent>
-                    <PlaylistCommand playlists={playlists} item={item} />
-                  </PopoverContent>
-                </Popover>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <PlaylistItemTable items={items} playlists={playlists} />
     </div>
   )
 }
